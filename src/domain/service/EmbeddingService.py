5b1a0acd1602f2fb0ff16ff7fe0864221ff43e1e
Miguel de  Oliveira <migvanderlei@gmail.com>
2022-05-20 00:08:11 -0400

    feat: creating RecommendationService

---
+++ src/domain/service/EmbeddingService.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from sentence_transformers import SentenceTransformer
+import numpy as np
+
+CURRENT_PATH = str(Path(__file__).resolve().parent)
+DATA_PATH = CURRENT_PATH+'/../../../data'
+FILENAME = 'embeddings.npy'
+
+class EmbeddingService():
+    def __init__(self, lazy_setup=True, model_name='distilbert-base-nli-mean-tokens'):
+        self.lazy_setup = lazy_setup
+        self.model_name = model_name
+
+        if not lazy_setup:
+            self.setup()
+
+    def setup(self):
+        self.model = SentenceTransformer(self.model_name)
+
+    def build_embeddings(self, X):
+        print("Encoding process started")
+
+        if self.lazy_setup:
+            self.setup()
+
+        embeddings = self.model.encode(X)
+        np.save(DATA_PATH+'/'+FILENAME, embeddings)
+
+        return embeddings
+
+    def load_embeddings(self):
+        try:
+            return np.load(DATA_PATH+'/'+FILENAME)
+        except FileNotFoundError:
+            return None
---
+++ src/domain/service/RecommendationService.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import cosine_similarity
+from src.domain.entity.Artist import Artist
+
+from src.infra.repository.ArtistRepository import ArtistRepository
+from src.infra.client.MongoDbClient import MongoDbClient
+
+CURRENT_PATH = str(Path(__file__).resolve().parent)
+DATA_PATH = CURRENT_PATH+'/../../../data'
+FILENAME = 'cosine_similarity.csv'
+
+class RecommendationService:
+
+    def __init__(self):
+        self.repository = ArtistRepository(MongoDbClient())
+
+
+    def load_dataframe(self) -> pd.DataFrame:
+        all_data = self.repository.get_all()
+
+        index = [data.get('id') for data in all_data]
+
+        df = pd.DataFrame(all_data, index=index)
+
+        return df
+
+    def get_texts(self, df: pd.DataFrame) -> np.ndarray:
+        return np.array(df.description)
+
+    def compute_cosine_similarity(self, embeddings: np.ndarray, index) -> pd.DataFrame:
+        cosine_similarity_data = pd.DataFrame(
+            cosine_similarity(embeddings)
+        )
+
+        cosine_similarity_data = cosine_similarity_data.set_index(index)
+        cosine_similarity_data.columns = index.to_list()
+        
+        cosine_similarity_data.to_csv(DATA_PATH+'/'+FILENAME)
+
+        return cosine_similarity_data
+    
+    def load_cosine_similarity(self) -> pd.DataFrame:
+        cosine_similarity_data = pd.read_csv(DATA_PATH+'/'+FILENAME, index_col=0)
+
+        return cosine_similarity_data
+
+    def recommend(self, index, cosine_similarity_data):
+        print(cosine_similarity_data.index)
+        recommendation = cosine_similarity_data.loc[index].sort_values(ascending=False).index.tolist()[1:6]
+
+        return recommendation
+ 
